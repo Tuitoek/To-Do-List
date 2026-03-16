db65dec5edea639d295ea6bc7d2c316e6010c5a9
Tuitoek <sjtoek@gmail.com>
2026-03-16 11:00:15 +0300

    Add delete button functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 let add = document.getElementById("add");
 let form = document.getElementById("form");
 
+// Array for storing tasks
+let tasks = [];
+
 // Eventlistener for toggling the input div
 add.addEventListener('click', function (event) {
     if (form.style.display === "none") {
@@ -12,18 +15,38 @@ add.addEventListener('click', function (event) {
 });
 
 // Eventlistener for capturing submit data
-function addTask(event){
-    let task = document.getElementById("do").value;
-    console.log(task);
+function addTask(event) {
+    let newTask = document.getElementById("do").value;
+    tasks.push(newTask);
+    console.log(newTask);
 
     // Create an element for posting ordered data
-    let taskLink = document.querySelector(".tasklist li");
+    let taskLink = document.querySelector(".tasklist li span");
     let li = document.createElement("li");
-    li.textContent = ". " + task;
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("border-1", "border-solid", "p-1", "font-mono", "text-sm", "ml-4");
+    deleteBtn.addEventListener('click', deleteTask);
 
+    // Numbering the tasks
+    let taskNumber = tasks.length;
+
+    li.textContent = taskNumber + ". " + " " + newTask;
+
+    //Append the new task to the list and the delete button to the task
     taskLink.appendChild(li);
+    taskLink.appendChild(deleteBtn);
 
     // Prevent Form from refreshing once submitted
     event.preventDefault();
 };
+
+// Eventlistener for deleting the last task
+function deleteTask(event) {
+    let taskLink = document.querySelector(".tasklist li");
+    taskLink.removeChild(taskLink.lastChild);
+    tasks.pop();
+    console.log(tasks);
+    event.preventDefault();
+}
 
